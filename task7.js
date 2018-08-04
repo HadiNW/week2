015c10ba76081f48973dcae444b7f33bfa0d65f4
Hadi NW <hadinw@Hadis-MacBook-Pro.local>
2018-08-04 12:29:00 +0700

    a

--- task7.js
+++ task7.js
@@ -1,13 +1,25 @@
 // 1. Menyusun Barisan Bintang
-// for (var row =1; row <6; row++) {
-//     console.log('*');
-// }
+console.log('1. Menyusun Barisan Bintang');
+for (var row =1; row <6; row++) {
+    console.log('*');
+}
 
 
 // 2. Menyusun Barisan Bintang Dengan Nested Looping
+console.log('2. Menyusun Barisan Bintang Dengan Nested Looping');
+
+var rows2 = 5;
+var str = '';
+for (var counter = 1; counter <= rows2; counter++) {
+    str = str+'*';
+}
+for (var loop = 1; loop <= rows2; loop++) {
+    console.log(str);
+}
 
 
 // 3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+console.log('3. Menyusun Barisan Tangga Bintang Dengan Nested Looping');
  
 var row3 = 5;   
 for (var count = 1; count <= row3; count++) { // 2
